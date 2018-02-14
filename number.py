2dc7ab77a711637d552f6ecd43e79e5ddb9cc754
System Administrator <root@lon-m-t19492-8.ad.imagination.com>
2018-02-14 17:35:42 +0000

    first lesson

---
+++ number.py
@@ -0,0 +1,6 @@
+from __future__ import division
+# The above line will import the python 3 module and it will enable true division in python 2.
+print(3*2)
+print(3/2)
+print(float(3)/2)
+print 2**3
