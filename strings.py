215b6332c9f9ea285470cc4938125e8fb11ba729
Mohammad Ayub <mohammad.ayub@imagination.com>
2018-02-15 10:11:31 +0000

    added string.py

---
+++ strings.py
@@ -0,0 +1,18 @@
+# In Python strings are represented by either single quote or double quote.
+# If we are having a single quote in our string and we want to keep that, the we must use double quote.
+my_string = "Hello"
+print my_string
+print my_string[0]
+print my_string[3]
+
+# In Python there is reverse indexing. If we want to grab the last character of a string, but we don't know the length
+# of the string, then all we have to do is use -1 for the index of the last character. Similarly the second last
+# will be -2 and it continues like this.  This is called reverse indexing.
+
+print my_string[-1]
+
+# We can easily reverse a string by using the syntax [::-1], this states to give all the indexes starting from -1, i-e
+# the last character in the string.
+print my_string[::-1]
+
+print my_string[0:-1:2]
--- variables.py
+++ variables.py
@@ -1,3 +1,19 @@
 # Python is dynamically typed, it means that we can assign any type of value to a variable.
 # Unlike Java which is statically typed. Which means we have to declare the type of the variable and can only
 # assign that type of value to it.
+a = 5
+print a
+a = a + a
+
+print a
+
+a = 'b'
+# We can use the type function to determine the type of the variable a in the following example.
+# We will get string as a currently have the value 'b'. If however we will change it to an int then the type() function
+# will return the type as int
+print type (a)
+
+my_income = 100
+my_tax_rate = 0.40
+my_tax = my_income * my_tax_rate
+print my_tax
