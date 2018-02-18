4462d31a4fa6d2265176559a49156be7f1bbc9e8
Mohammad Ayub <mohammad.ayub@harpercollins.co.uk>
2018-02-18 12:21:26 +0000

    added dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,2 +1,9 @@
 # Dictionaries are unordered mappings for storing objects.  
 # The key-value pair allows users to quickly grab objects without needing to know an index location.
+# Dictionary uses {} and colons to signify the keys and their associated values.
+my_dic = {'key1':'value', 'key2':'value2'}
+print (my_dic)
+print (my_dic['key1'])
+
+my_other_dic = {2:3,4:5}
+print(my_other_dic[2])
