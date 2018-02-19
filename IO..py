8a4de3521c3cf27c12d876187f34f5f9b8e3d885
Mohammad Ayub <mohammad.ayub@harpercollins.co.uk>
2018-02-19 07:42:13 +0000

    added io.py

---
+++ IO..py
@@ -0,0 +1,26 @@
+# If the textfile doesn't exist or the path to the file is incorrect then the following function will throw an error.
+my_file = open('test.txt')
+my_file.read
+my_file.seek(0)
+file_content = my_file.read()
+print(file_content)
+# After the read command above the cursor is now at the end of the file and hence a nother read command will return empty string.
+# To move the cursor back to the begining we use the following command.
+my_file.seek(0)
+readline_content = my_file.readline()
+print(type(readline_content))
+print(readline_content)
+my_file.seek(0)
+
+readlines_content = my_file.readlines()
+print(type(readlines_content))
+print(readlines_content)
+
+# As best practice it is important to close the file. If we will forget to close the file we will get error if we
+# wanted to delte the file etc as this file will be in use in python.
+my_file.close()
+
+# To avoid the above problems the best solution is to open the file as follows.
+with open('test.txt') as my_new_file:
+    contents = my_new_file.read()
+    print(contents)
