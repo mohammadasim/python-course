456b3eb63c02534110532178e1e24e86271d4deb
Mohammad Ayub <mohammad.ayub@harpercollins.co.uk>
2018-02-20 07:43:42 +0000

    added test.py

---
+++ object_data_str_test.py
@@ -0,0 +1,25 @@
+# Solutions for python test
+# Numbers are basic data structures. Two types int and floating point. 
+# Strings are indexed data structure. Immutable.
+# List are ordered collection of elements. Different types of elements can be contained in the same set.
+# Tuples are immutable, with limited functinality in python. They are indexed too.
+# Dictionaries are un ordered collection of key value pair.
+print (((2**5)*10/10)-2+70.25)
+#44
+print(4*(6+5))
+#29
+print(4*6+5)
+#34
+print(4+6*5)
+#Floating point number
+result = 3+1.5+4
+print(type(result))
+
+s = 'hello'
+print(s[1])
+print(s[::-1])
+print(s[4])
+print(s[-1])
+
+my_list = 3*[0]
+print(my_list)
