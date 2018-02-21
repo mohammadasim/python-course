524480bc5f2f531b80634b4bf55065f5fec7d94f
Mohammad Ayub <mohammad.ayub@harpercollins.co.uk>
2018-02-21 07:40:03 +0000

    added comparison.py

---
+++ comparison.py
@@ -0,0 +1,16 @@
+# When comparing strings the capital matters.
+print('Bye' == 'bye')
+
+# Similarly '2' is not equal to 2
+
+print ('2' == 2)
+
+# In python the comparison operator is 'and' unlike java where it is &&
+print(('h' == 'h') and (2 == 2))
+
+# In python the comparison operatior is 'or' unlike java whre it is |
+print(100 == 1 or 2 == 2)
+
+# In python 'not' comparison operator will return the opposite of what was going to be returned originally
+print(not 4 == 5)
+
