fc26d83c30a94ee4a0f17e169b157aa63ed1ec54
Mohammad Ayub <mohammad.ayub@harpercollins.co.uk>
2018-02-21 07:45:34 +0000

    added statement.py

---
+++ statement.py
@@ -0,0 +1,5 @@
+# Control flow syntax makes use of colons and indentation (whitespace)
+# This indentation system is crucial to python and is what sets it apart from other programming languages.
+# If a condition is true we execute a piece of code, we then put else, followed by code if the condition is false this piece
+# of code will be executed. However if we want to check for multiple conditions before the else part can be executed
+# For this we follow the if part with an elif statement that also has a conditional check.
