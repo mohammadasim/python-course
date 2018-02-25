bc3ac940d12175a0da5710a2f0ca543fc56317e5
Mohammad Ayub <mohammad.ayub@harpercollins.co.uk>
2018-02-25 10:23:26 +0000

    added operators.py

--- operators.py
+++ operators.py
@@ -17,4 +17,38 @@ for item in enumerate(word):
     print(item)
 
 
-# Zip 
+# Zip is used to combine lists as tuples in a single list. 
+# If 1 list is longger than the other the zip will go as far as the shortest list and will ignore the rest of the element of 
+# of the long list.
+print('Following is the result of zip')
+list_1 = [1,2,3,4,5]
+list_a = ['x','y','z']
+for item in zip(list_1,list_a):
+    print (item)
+
+# You can cast the result of zip into a list
+print(list(zip(list_1,list_a)))
+
+# 'in' is a key word that is used to check if the presence of an element in an interable object i-e string list, or even dictionary.
+# we are already using it in for loop.
+print('a' in 'abcd')
+print(1 in [8,9,7])
+d = {'k1':'v1','k2':'v2','k3':'v3'}
+print('k1' in d.keys())
+print('v8' in d.values())
+
+# min() and max() functions in python returns the minimum and maximum values of a list respectively.
+print(min([1,2,3,45]))
+print(max([1,2,3,45]))
+
+# Python comes with a random library. we have to import this library
+from random import shuffle
+new_list = [1,2,3,4,6,7,5,8,9,10]
+shuffle(new_list)
+print(new_list)
+from random import randint
+print(randint(1,1000000))
+
+# How to accept user input
+name = input('What is your name: ')
+print('hello {}'.format(name))
