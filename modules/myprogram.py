a070dde18709f706c065e8f9af0eb48929c40662
Mohammad Ayub <mohammad.ayub@harpercollins.co.uk>
2018-04-05 07:04:58 +0100

    added module folder.

---
+++ modules/myprogram.py
@@ -0,0 +1,11 @@
+# Import from another module example
+from mymodule import my_func
+# Import from another package example
+from MyMainPackage import some_main_script
+# Import from a subpackage example
+from MyMainPackage.SubPackage import my_sub_script
+
+my_func()
+
+some_main_script.report_main()
+my_sub_script.sub_report()
