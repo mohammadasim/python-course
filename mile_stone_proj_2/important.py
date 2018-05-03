1e3c647389d0e7b8f0367a381b668e6d520c94f6
Mohammad Ayub <mohammad.ayub@harpercollins.co.uk>
2018-05-03 07:05:51 +0100

    added important.py to note an important point

---
+++ mile_stone_proj_2/important.py
@@ -0,0 +1,19 @@
+'''
+In python 0 is treated as false and other numbers are treated as true.
+Let's check in the following example.
+'''
+
+zero = 0
+one = 1
+two = 2
+
+if zero:
+    print('You were wrong, zero is not treated as true')
+else:
+    print('You were right, zero is treated a false')
+
+if one:
+    print('1 is treated as true')
+
+if two:
+    print('2 is treated as true')
