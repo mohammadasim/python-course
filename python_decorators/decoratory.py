5e3108323101329851365de8ff2cd8c4fa87ca77
Mohammad Ayub <mohammad.ayub@harpercollins.co.uk>
2018-05-21 06:47:17 +0100

    completed decorator.py

--- python_decorators/decoratory.py
+++ python_decorators/decoratory.py
@@ -40,13 +40,78 @@ def hello(name='Asim'):
     def welcome():
         return '\t this is welcome() inside hello'
     
-    print(greet())
-    print(welcome())
-    print('This is the end of hello function')
+    print('I am going to return a function!!')
+
+    if name == 'Asim':
+        return greet
+    else:
+        return welcome
 '''
 Important point to notice is that greet() and welcome functions are only visible inside the hello() function.
 Outside these functions these functions are not visible. so how can we access these functions.  Well if we can make 
 hello() to return a function then it will work. So the hello() function will be modified to return the greet or welcome function 
 rather then just using the print statement.
 '''
-print(hello())
+#my_new_func = hello()
+#print(my_new_func)
+#print(my_new_func())
+
+def cool():
+    def super_cool():
+        return 'I am very cool!!'
+    return super_cool
+
+#some_func = cool()
+#print(some_func())
+
+
+def hello():
+    return 'Hi Asim'
+
+def other(some_def_func):
+    '''
+    Unlike before now we are passing function as an agrument
+    we then execute some code and then exectue the function 
+    passed as an argument.
+    '''
+    print('Other code runs here!')
+    print(some_def_func())
+
+# It is important to notice that we are passing hello not hello(), because hello() is execution and 
+# we don't want to execute the function, the execution will be done inside the other()
+print(other(hello))
+
+
+def new_decorator(original_func):
+
+    def wrap_func():
+        '''
+        The idea is to take a function, add extra functionality to it and then return it
+        All the above examples are to prepare for the decorator and to understand it better.
+        '''
+        print('Some extra code, before the original function')
+        original_func()
+        print('Some extra code, after the original function!')
+
+    return wrap_func
+
+def func_needs_decorator():
+    print('I want to be decorated!!')
+'''
+ Below is the long way of doing things. 
+'''
+#decorated_func = new_decorator(func_needs_decorator)
+#print(decorated_func())
+
+'''
+The shortest way is as follows
+'''
+@new_decorator
+def func_needs_decorator():
+    '''
+    This decorator at the top acts like a switch, if it is active the extra functionality is added
+    to the function, if it is commented off or removed the extra functionaltiy is removed.
+    '''
+    print('I want to be decorated!!')
+
+print(func_needs_decorator())
